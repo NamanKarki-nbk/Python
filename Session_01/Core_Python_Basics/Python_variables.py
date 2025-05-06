54d14334676175fca5d941bc5f3973c20e76b95b
NamanKarki-nbk <namankarki.nbk@gmail.com>
2025-05-06 12:19:45 +0545

    session-o1

---
+++ Session_01/Core_Python_Basics/Python_variables.py
@@ -0,0 +1,137 @@
+# x =4
+# y ="hello"
+# z =str(3.5)
+# f =True
+
+# print(x)
+# print(y)
+# print(type(z))
+# print(f)
+
+# x = "Python "
+# y = "is "
+# z = "awesome"
+# print(x + y + z)
+
+# x = 5
+# y = "John"
+# print(x, y)
+
+# z = " Naman"
+
+# def fyfunc():
+#     print("My name is" + z)
+    
+# fyfunc()
+
+#---------------------------------------------#
+
+# b= "Hello my name is Naman Karki"
+# print(b[17:28])
+
+# z="Max1ie"
+# print(z[-5:-2])
+# print(z.replace("e", "i"))
+# print(b.split(" "))
+
+# #negative indexing starts from 1 not from 0
+
+
+# day=4
+# match day:
+#     case 1:
+#         print("Monday")
+#     case 2:
+#         print("Tuesday")
+#     case 3:
+#         print("Wednesday")
+#     case 4:
+#         print("Thursday")
+#     case 5:
+#         print("Friday")
+#     case 6:
+#         print("Saturday")
+#     case 7:
+#         print("Sunday")
+        
+        #default value _ if there is no mathcing value case _: 
+        
+
+# a = 45
+# b=18
+# if(a<50):
+#     print("Ke cha 45")
+# elif(b>50):
+#     print("thukka torpe")
+# else:
+#     print("Tumso na ho pai")
+
+
+# i = 0
+# while i < 10:
+#     print(i)
+#     i += 1  # increment operator
+
+# i = 0
+# while i < 10:
+#     # print(i)
+#     i += 1
+#     if i == 7:
+#         continue //skip hancha
+#     print(i)
+
+# i = 0
+# while i < 5:
+#     print(i)
+#     if i == 3:
+#         break //yesle chai loop lai stop garcha on certain condition
+#     i += 1
+    
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+# else:
+#   print("i is no longer less than 6")
+
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#     print(x)
+
+# for x in "banana":
+#     print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+#   if x == "banana":
+#     break
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     break
+#   print(x)
+
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   if x == "banana":
+#     continue
+#   print(x)
+  
+# for x in range(6):
+#   print(x)
+
+# for x in range(2, 30, 3):
+#   print(x)
+
+# car=["mercedies", "tesla", "porshe", "bmw"]
+# color=["black", "white", "yello", "blue"]
+
+# for x in car:
+#     for y in color:
+#         print("The car and its color is: " + x,y)
+
+# for x in [0, 1, 2]:
+#   pass
